3c4fdd5a97e54080543e273bc371048d4f76667d
roheat <rohithsrivathsav@gmail.com>
2020-03-13 00:33:00 +0530

    folder structure updated

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
-import Home from "./home.component";
-function Bikes() {
-  return (
-    <div>
-      <h1>Bikes</h1>
-    </div>
-  );
-}
+import Home from "./pages/home/home.component";
+import Bikes from "./pages/bikes/bikes.component";
+
 function App() {
   return (
     <div>
---
+++ src/pages/bikes/bikes.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// import BIKES_DATA from './bikes.data.js';
+
+class Bikes extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: []
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>BIKESSS</h1>
+      </div>
+    );
+  }
+}
+
+export default Bikes;
