a3e5dd1bbc6b07921c46d2e4bc80e94090933b93
roheat <rohithsrivathsav@gmail.com>
2020-03-14 01:19:57 +0530

    images uploaded

--- src/pages/bikes/bikes.data.js
+++ src/pages/bikes/bikes.data.js
@@ -9,43 +9,35 @@ const BIKE_DATA = [
         name: "StumpJumper Pro 29",
         price: "6,300.00",
         partNumber: "No.94159-0001",
-        imageUrl: "../Assets/Images/PreviewCard/StumpJumperPro29-PrevCard.png"
+        imageUrl: "https://ibb.co/mH5ndP3"
       },
       {
         id: 2,
         name: "StumpJumper Expert 29",
         price: "5,000.00",
         partNumber: "No.94159-0002",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/WPhmxRK"
       },
       {
         id: 3,
         name: "StumpJumper Expert 27.5",
         price: "5,000.00",
         partNumber: "No.94159-0003",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/VLLqGc8"
       },
       {
         id: 4,
         name: "Fuse Expert 29",
         price: "2,100.00",
         partNumber: "No.94159-0004",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/rwZZHKz"
       },
       {
         id: 5,
         name: "Fuse Expert 27.5",
         price: "2,000.00",
         partNumber: "No.94159-0005",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/sQfvKq2"
       }
     ]
   },
@@ -59,27 +51,21 @@ const BIKE_DATA = [
         name: "Demo Racer 29",
         price: "6,500.00",
         partNumber: "No.94159-0006",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/HXcMxNB"
       },
       {
         id: 2,
         name: "Demo Expert 29",
         price: "6,300.00",
         partNumber: "No.94159-0007",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/tb7Y34Q"
       },
       {
         id: 3,
         name: "Demo Alloy 27.5",
         price: "3,400.00",
         partNumber: "No.94159-0008",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/Tmk6cb6"
       }
     ]
   },
@@ -93,45 +79,35 @@ const BIKE_DATA = [
         name: "S-Works Demo 8",
         price: "10,400.00",
         partNumber: "No.94159-0009",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/st9C8fq"
       },
       {
         id: 2,
         name: "S-Works Epic EVO",
         price: "9,500.00",
         partNumber: "No.94159-0010",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/t3fp9yg"
       },
       {
         id: 3,
         name: "S-Works StumpJumper 29",
         price: "9,520.00",
         partNumber: "No.94159-0011",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/cbwHMpS"
       },
       {
         id: 4,
         name: "S-Works StumpJumper 27.5",
         price: "9,520.00",
         partNumber: "No.94159-0012",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/Bf3N9Dj"
       },
       {
         id: 5,
         name: "S-Works Demo 8 Frame",
         price: "4,000.00",
         partNumber: "No.94159-0013",
-        imageUrl: "",
-        primaryColor: "#",
-        secondaryColor: "#"
+        imageUrl: "https://ibb.co/6XgHH6Q"
       }
     ]
   }
