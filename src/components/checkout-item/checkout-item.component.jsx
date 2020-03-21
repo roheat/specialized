4020e8426de8604e5287c8d622149ff7b53142ad
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2020-03-21 22:27:36 +0530

    checkout features

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,48 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import {
+  clearCartItem,
+  addCartItem,
+  removeCartItem
+} from "../../redux/cart/cart.actions";
 
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img alt="item" src={imageUrl} />
+const CheckoutItem = ({
+  cartItem,
+  clearCartItem,
+  addCartItem,
+  removeCartItem
+}) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img alt="item" src={imageUrl} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeCartItem(cartItem)}>
+          &#10094;
+        </div>
+        <div className="value">{quantity}</div>
+        <div className="arrow" onClick={() => addCartItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">${price}</span>
+      <span className="remove-button" onClick={() => clearCartItem(cartItem)}>
+        &#10005;
+      </span>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <span className="remove-button">&#10005;</span>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  addCartItem: item => dispatch(addCartItem(item)),
+  removeCartItem: item => dispatch(removeCartItem(item)),
+  clearCartItem: item => dispatch(clearCartItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
